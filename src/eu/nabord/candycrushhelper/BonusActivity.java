e57de51be72ca152868529b465291a24dc292475
Thomas Nabord <thomas.nabord@prestashop.com>
2014-09-29 14:38:50 +0200

    Added Bonus Activity

---
+++ src/eu/nabord/candycrushhelper/BonusActivity.java
@@ -0,0 +1,15 @@
+package eu.nabord.candycrushhelper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class BonusActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bonus);
+	}
+}
