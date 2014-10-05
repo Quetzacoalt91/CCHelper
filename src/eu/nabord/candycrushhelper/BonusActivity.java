09ccb38536e0ef79bf23ef6b1de39b78bd5ab0a2
Quetzacoalt91 <quetzacoalt@outlook.com>
2014-10-05 17:09:59 +0200

    Added others bonuses in activity

--- src/eu/nabord/candycrushhelper/BonusActivity.java
+++ src/eu/nabord/candycrushhelper/BonusActivity.java
@@ -23,7 +23,16 @@ public class BonusActivity extends Activity {
 		try {
 			HexReader file = new HexReader(Environment.getExternalStorageDirectory().getPath()+"/SaveCandyCrush/backup/save_1066067012.dat", "r");
 			
+			((EditText) findViewById(R.id.nbFreezeTime)).setText(file.getValueInFile("80").toString());
 			((EditText) findViewById(R.id.nbColorBomb)).setText(file.getValueInFile("50").toString());
+			((EditText) findViewById(R.id.nbJellyFish)).setText(file.getValueInFile("5C").toString());
+			((EditText) findViewById(R.id.nbCoconutWheel)).setText(file.getValueInFile("68").toString());
+			((EditText) findViewById(R.id.nbLollipopHammer)).setText(file.getValueInFile("74").toString());
+			((EditText) findViewById(R.id.nbLuckyCandy)).setText(file.getValueInFile("98").toString());
+			((EditText) findViewById(R.id.nbWrappedandStriped)).setText(file.getValueInFile("A4").toString());
+			((EditText) findViewById(R.id.nbFreeSwitch)).setText(file.getValueInFile("B0").toString());
+			((EditText) findViewById(R.id.nbMoonstruckBooster)).setText(file.getValueInFile("BC").toString());
+			((EditText) findViewById(R.id.nbLives)).setText(file.getValueInFile("290").toString());
 		} catch (FileNotFoundException e) {
 			Toast.makeText(getApplicationContext(), (String)e.getMessage(), 
 					   Toast.LENGTH_SHORT).show();
